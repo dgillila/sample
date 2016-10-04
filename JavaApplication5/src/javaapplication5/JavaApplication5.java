dfd761dadb8bb8e65be4ba0ffbd3800a02f961fb
Daniel <Daniel@192.168.1.144>
2016-10-03 18:36:34 -0600

    Merge branch 'master' of https://github.com/dgillila/sample
    # Conflicts:
    #	JavaApplication5/src/javaapplication5/JavaApplication5.java

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -20,6 +20,8 @@ public class JavaApplication5 {
         System.out.println("Hello again");
         System.out.println("Hello world.");
         System.out.println("Hello world.");
+        
+        System.out.println("Hello world.");
     }
     
 }
