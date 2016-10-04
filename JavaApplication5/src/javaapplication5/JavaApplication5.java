35c0e7014685cb663805d860d09eb384b7250278
Daniel <Daniel@192.168.1.144>
2016-10-03 18:32:08 -0600

    more updates

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -17,6 +17,8 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         System.out.println("Hello world.");
         System.out.println("Hello again");
+        System.out.println("Hello again");
+        System.out.println("Hello world.");
     }
     
 }
