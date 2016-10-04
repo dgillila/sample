b51d78b944a26403d4984d977a83c719aa749450
Daniel <Daniel@192.168.1.144>
2016-10-03 18:40:22 -0600

    dog class complete

--- JavaApplication5/src/javaapplication5/Dog.java
+++ JavaApplication5/src/javaapplication5/Dog.java
@@ -5,13 +5,36 @@
  */
 package javaapplication5;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Daniel
  */
-public class Dog {
+public class Dog implements Serializable {
     
     private String name;
     private int age;
+    
+    public Dog() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     
 }
